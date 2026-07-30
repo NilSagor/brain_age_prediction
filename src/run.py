11d2d17ad324ca09696f75c9d86c9a9a51046140
NilSagor <masum1201@gmail.com>
2026-07-30 12:17:43 +0800

    feat:abidedataset donwloading script in run.py

---
+++ src/run.py
@@ -0,0 +1,16 @@
+from src.NeuroFusion.data import ABIDEDataModule
+
+# For development/validation on ABIDE
+data_module = ABIDEDataModule(
+    data_dir='./abide_data',
+    batch_size=8,
+    age_min=18,
+    age_max=64,
+    roi_atlas='cc200',
+    quality_checked=True,
+)
+
+data_module.setup()
+train_loader = data_module.train_dataloader()
+batch = next(iter(train_loader))
+print(batch.keys())
